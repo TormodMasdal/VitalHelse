c2f50782a8b2f4de59e64d73647d308bd59f7d93
Tormod Masdal <tormod@masdal.com>
2025-10-19 12:53:41 +0200

    Added gitignore

---
+++ VitalHelse/Areas/Identity/Pages/Account/BusinessRegister.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VitalHelse.Areas.Identity.Pages.Account;
+
+public class BusinessRegister : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
