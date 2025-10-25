f92ff552ec2c0bd23f71c751d43621ea012f35b7
Oline Kylland Sæther <olineks@uia.no>
2025-10-25 14:56:01 +0200

    added files for profilecontroller and view for private customer

--- VitalHelse/Controllers/ProfileController.cs
+++ VitalHelse/Controllers/ProfileController.cs
@@ -1,23 +1,13 @@
-namespace VitalHelse.Controllers;
-
-using Microsoft.AspNetCore.Identity;
 using VitalHelse.Data;
 using VitalHelse.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace VitalHelse.Controllers;
 
 public class ProfileController : Controller
 {
-    private readonly ApplicationDbContext _db;
-    private readonly ILogger<ProductController> _logger;
-    private readonly UserManager<AspNetUsers> _um;
-    public ProfileController(ApplicationDbContext db, ILogger<ProductController> logger, UserManager<AspNetUsers> um)
-    {
-        _db = db;
-        _logger = logger; 
-        _um = um;
-    }
-
-    public IActionResult PrivateCustomer()
+    public IActionResult privateCustomer()
     {
         return View();
     }
