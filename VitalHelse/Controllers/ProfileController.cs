72c9ed2ba5a0363e76132f3fc1bbf49efbea375e
Emanuel Rønningsland <emanuelr@uia.no>
2025-10-28 12:37:42 +0100

    Rebased profilepage into main

--- VitalHelse/Controllers/ProfileController.cs
+++ VitalHelse/Controllers/ProfileController.cs
@@ -1,10 +1,9 @@
+namespace VitalHelse.Controllers;
+
 using Microsoft.AspNetCore.Identity;
 using VitalHelse.Data;
 using VitalHelse.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-
-namespace VitalHelse.Controllers;
 
 public class ProfileController : Controller
 {
@@ -17,7 +16,8 @@ public class ProfileController : Controller
         _logger = logger; 
         _um = um;
     }
-    public IActionResult PrivateCustomer()
+
+    public IActionResult privateCustomer()
     {
         return View();
     }
