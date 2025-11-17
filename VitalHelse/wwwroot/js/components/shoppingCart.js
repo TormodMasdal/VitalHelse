a1716b9a0673760e7f470935f28490736d80113e
Oline Kylland Sæther <olineks@uia.no>
2025-11-17 10:22:18 +0100

    removed go to cart popup

--- VitalHelse/wwwroot/js/components/shoppingCart.js
+++ VitalHelse/wwwroot/js/components/shoppingCart.js
@@ -52,7 +52,7 @@ async function AddToCart(productId){
         if (!isAuthenticated) {
             window.location.href = "/identity/account/login";
             return;
-        }
+        }/*
         const popup = document.getElementById("cart-popup");
         popup.style.display = "block";
         document.getElementById("go-to-cart").onclick = () => {
@@ -60,7 +60,8 @@ async function AddToCart(productId){
         };
         document.getElementById("continue-shopping").onclick = () => {
             popup.style.display = "none";
-        };
+        }; */
+        window.location.reload();
     }
 }
 
